0f901fc6f346a1c579d47ba136bf279cd2639e86
DaRa <damian.rauchenstein@outlook.com>
2023-03-29 23:40:39 +0200

    Update myToday.h

--- myToday.h
+++ myToday.h
@@ -8,10 +8,10 @@
 void printTime() {
 	char timeString[16];
 
-	sprintf(timeString, " %02d:%02d ", tm.tm_hour, tm.tm_min);
+	sprintf(timeString, " %02d:%02d ", timevar.tm_hour, timevar.tm_min);
 
 	myToday.createCanvas(330, 100);
-	myToday.setTextSize(92);
+	myToday.setTextSize(fontSize);
 	myToday.setTextDatum(TC_DATUM);
 	myToday.drawString(timeString, 165, 5);
 	myToday.pushCanvas(5, 60, UPDATE_MODE_GC16);
@@ -19,7 +19,7 @@ void printTime() {
 
 	myToday.createCanvas(200, 80);
 	myToday.fillCanvas(MYWHITE);
-	myToday.setTextSize(18);
+	myToday.setTextSize(fontSize);
 	drawBattery(myToday, 130, 10);
 	drawRSSI(myToday, 130, 45);
 	myToday.pushCanvas(750, 410, UPDATE_MODE_GC16);
@@ -31,12 +31,12 @@ void printDate() {
 	char buf1[64];
 	char buf2[30];
 
-	sprintf(dateString, "%s, %02d. %s %04d", dayNames[tm.tm_wday], tm.tm_mday, monthNames[tm.tm_mon + 1], tm.tm_year + 1900);
-	strftime(buf1, sizeof(buf1), "KW: %V / Tag im Jahr: %j", &tm);
-	strftime(buf2, sizeof(buf2), "Zeitzone: %Z", &tm);
+	sprintf(dateString, "%s, %02d. %s %04d", dayNames[timevar.tm_wday], timevar.tm_mday, monthNames[timevar.tm_mon + 1], timevar.tm_year + 1900);
+	strftime(buf1, sizeof(buf1), "KW: %V / Tag im Jahr: %j", &timevar);
+	strftime(buf2, sizeof(buf2), "Zeitzone: %Z", &timevar);
 
 	myToday.createCanvas(595, 115);
-	myToday.setTextSize(36);
+	myToday.setTextSize(fontSize);
 	myToday.setTextDatum(TL_DATUM);
 	myToday.drawString(dateString, 5, 0);
 	myToday.drawString(buf1, 5, 38);
@@ -45,7 +45,7 @@ void printDate() {
 	myToday.deleteCanvas();
 
 	myToday.createCanvas(600, 120);
-	myToday.setTextSize(24);
+	myToday.setTextSize(fontSize);
 	myToday.setTextDatum(TL_DATUM);
 	sprintf(buf1, "Netzwerk: %s", WiFi.SSID());
 	myToday.drawString(buf1, 5, 0);
@@ -64,10 +64,10 @@ void printFrame() {
 	myToday.createCanvas(960, 540);
 
 	myToday.fillRect(0, 0, 960, 30, MYBLACK);
-	myToday.setTextSize(24);
+	myToday.setTextSize(3);
 	myToday.setTextDatum(TC_DATUM);
 	myToday.setTextColor(MYWHITE, MYBLACK);
-	myToday.drawString("--{ Heute }--", 480, 3);
+	myToday.drawString("Heute", 480, 3);
 
 	myToday.setTextColor(MYBLACK, MYWHITE);
 	myToday.drawFastHLine(0, 0, 960, 3, MYBLACK);
@@ -77,7 +77,7 @@ void printFrame() {
 	myToday.drawFastVLine(0, 0, 540, 3, MYBLACK);
 	myToday.drawFastVLine(956, 0, 540, 3, MYBLACK);
 	myToday.drawFastVLine(350, 30, 320, 2, MYBLACK);
-	myToday.setTextSize(24);
+	myToday.setTextSize(fontSize);
 	myToday.setTextDatum(TL_DATUM);
 	myToday.drawString("Zeit: ", 5, 35);
 	myToday.drawString("Datum: ", 355, 35);
@@ -86,10 +86,10 @@ void printFrame() {
 	myToday.drawString("System-Infos: ", 5, 355);
 
 	myToday.fillRect(0, 510, 960, 30, MYBLACK);
-	myToday.setTextSize(24);
+	myToday.setTextSize(fontSize);
 	myToday.setTextDatum(TC_DATUM);
 	myToday.setTextColor(MYWHITE, MYBLACK);
-	myToday.drawString("\u00a9 TomSoft " + String(tm.tm_year + 1900), 480, 513);
+	//myToday.drawString("\u00a9 TomSoft " + String(timevar.tm_year + 1900), 480, 513);
 	myToday.setTextColor(MYBLACK, MYWHITE);
 
 	myToday.pushCanvas(0, 0, UPDATE_MODE_GC16);
@@ -130,16 +130,16 @@ void printWeather() {
 	else if (icon == "50n") drawIcon(myToday, iconX, iconY, (uint16_t*)image_data_50n, 64, 64, true);
 	else drawIcon(myToday, iconX, iconY, (uint16_t*)image_data_unknown, 64, 64, true);
 
-	myToday.setTextSize(36);
+	myToday.setTextSize(fontSize);
 	myToday.setTextDatum(TR_DATUM);
 	myToday.drawString("A: " + getFloatString(weather.hourlyMaxTemp[0], " \u00b0C"), 330, 5);
 	myToday.drawString("I: " + String(sht30Temperatur) + " \u00b0C", 330, 45);
 
 	myToday.setTextDatum(TL_DATUM);
-	myToday.setTextSize(24);
+	myToday.setTextSize(fontSize);
 	myToday.drawString(weather.hourlyMain[0], 5, 80);
 
-	myToday.setTextSize(14);
+	myToday.setTextSize(fontSize);
 	myToday.setTextDatum(TL_DATUM);
 	strftime(buf, sizeof(buf), "Stand: %d.%m.%Y %H:%M:%S", &weatherTime);
 	myToday.drawString(buf, 5, 110);
@@ -149,8 +149,9 @@ void printWeather() {
 
 }
 
+String startdat;
 void printNextEvent() {
-	byte p = 20;
+	int p = 20;
 	int checkDay;
 	int checkMonth;
 	int checkYear;
@@ -159,25 +160,58 @@ void printNextEvent() {
 
 	rTC = readCalendar();
 
-	Serial.println("Calendar OK? " + String(rTC));
+	Serial.println("Calendar OKZ? " + String(rTC));
 
-	tmElements_t currentNow = { 0, tm.tm_min, tm.tm_hour, 0, tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900 };
+	tmElements_t currentNow = { 0, timevar.tm_min, timevar.tm_hour, 0, timevar.tm_mday, timevar.tm_mon + 1, timevar.tm_year + 1900 };
 	time_t currentNowTime = makeTime(currentNow);
+//String mynow( timevar.tm_mday + "." + timevar.tm_mon + "." + timevar.tm_year);
+	//Serial.println("CurrentNow: " + mynow );
+Serial.println("blaaaa");
+String xx;
+	for (int r = 0; r < 11; r++) {
 
-	//Serial.println("CurrentNow: " + String(currentNowTime));
-
-	for (byte r = 0; r < 11; r++) {
+    xx = String( "startdate:" ) + calEnt[r].calStartDate + "Y";
+Serial.println( xx );
+    /*
 		checkDay = calEnt[r].calStartDate.substring(0, 2).toInt();
 		checkMonth = calEnt[r].calStartDate.substring(3, 5).toInt();
 		checkYear = calEnt[r].calStartDate.substring(6, 10).toInt();
 		checkHour = calEnt[r].calStartTime.substring(0, 2).toInt();
 		checkMin = calEnt[r].calStartTime.substring(3, 5).toInt();
 
-		//Serial.println(String(checkMin) + " " + String(checkHour) + " " + String(checkDay) + " " + String(checkMonth) + " " + String(checkYear));
+		checkDay = calEnt[r].calStartDate.substring(0, 2).toInt();
+		checkMonth = calEnt[r].calStartDate.substring(3, 5).toInt();
+		checkYear = calEnt[r].calStartDate.substring(6, 10).toInt();
+		checkHour = calEnt[r].calStartTime.substring(0, 2).toInt();
+		checkMin = calEnt[r].calStartTime.substring(3, 5).toInt();
+*/
+
+
+		checkDay = calEnt[r].calStartDate.substring(0, 2).toInt();
+		checkMonth = calEnt[r].calStartDate.substring(3, 5).toInt();
+		checkYear = calEnt[r].calStartDate.substring(6, 10).toInt();
+		checkHour = calEnt[r].calStartTime.substring(0, 2).toInt();
+		checkMin = calEnt[r].calStartTime.substring(3, 5).toInt();
+
+//String Datestring = String(checkMin) + "/" + String(checkHour + "/" + String(checkDay) + "/" + String(checkMonth) + "/" + String(checkYear);
+String Datestring =String( checkDay ) + String( checkMonth ) + String( checkYear ) ;
+
+ //Serial.println(Datestring && 'Afterstring' );
+/*
+startdat = calEnt[r].calStartDate;
+Serial.println(startdat);
+		checkDay = calEnt[r].calStartDate.substring(0, 2).toInt();
+		checkHour = calEnt[r].calStartDate.substring(3, 5).toInt();
+		checkYear = calEnt[r].calStartTime.substring(6, 10).toInt();
+		checkMonth = calEnt[r].calStartTime.substring(0, 2).toInt();
+		checkMin = calEnt[r].calStartTime.substring(3, 5).toInt();
+Serial.println("blaaaa3");
+*/
 
 		tmElements_t calEvent = { 0, checkMin, checkHour, 0, checkDay, checkMonth, checkYear };
 		time_t calEventTime = makeTime(calEvent);
 
+Serial.println( String( calEnt[r].calStartDate ) );
 		//Serial.println("calEventTime: " + String(calEventTime));
 
 		if (currentNowTime <= calEventTime && calEnt[r].calAllDay == "false") {
@@ -185,23 +219,27 @@ void printNextEvent() {
 			break;
 		}
 		if (calEnt[r].calStartDate == "") break;
+
+p = r;
 	}
+//Serial.println(startdat);
 
 	//Serial.println("p = " + String(p));
 
 	myToday.createCanvas(595, 135);
 
-	myToday.setTextSize(36);
+	myToday.setTextSize(fontSize);
 	myToday.setTextDatum(TL_DATUM);
 	if (!rTC) {
 		myToday.drawString("Fehler beim Holen der Daten", 5, 20);
 	}
+  
 	else if (p == 20) {
 		myToday.drawString("Kein Termin in der n\u00e4chsten Zeit", 5, 20);
 	}
 	else {
 		myToday.drawString(calEnt[p].calTitle, 5, 10);
-		myToday.setTextSize(24);
+		myToday.setTextSize(fontSize);
 		if (calEnt[p].calLocation != "") {
 			iconLocation(myToday, 5, 50, 25);
 			myToday.drawString(calEnt[p].calLocation, 35, 50);
